b06b6bb60ab495d620f94fcc82a16ba7c0f24930
szkan <71358350+szkan@users.noreply.github.com>
2020-10-07 18:15:27 +0900

    出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :shipment_day
+  belongs_to_active_hash :shipment_source
+
   validates :name, presence: true
   validates :text, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_id, presence: true
-  validates :shipment_source_id, presence: true
-  validates :shipment_day_id, presence: true
-  validates :price, presence: true, format: { with: /\d[300-9999999]/ }
+  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :condition_id, presence: true, numericality: { other_than: 1 }
+  validates :postage_id, presence: true, numericality: { other_than: 1 }
+  validates :shipment_source_id, presence: true, numericality: { other_than: 1 }
+  validates :shipment_day_id, presence: true, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   belongs_to :user
+  has_one_attached :image
 end

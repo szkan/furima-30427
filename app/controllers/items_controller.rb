242a3a72351ea204697ea85657ccd6005328d6b0
szkan <71358350+szkan@users.noreply.github.com>
2020-10-07 14:16:47 +0900

    商品出品のフロント側実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,24 @@
 class ItemsController < ApplicationController
   def index; end
+  def new
+    @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :postage_id, :shipment_source_id, :shipment_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end

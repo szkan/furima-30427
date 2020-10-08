fb0dd0e6ea3013a5981decc081e8f2351b04fb08
szkan <71358350+szkan@users.noreply.github.com>
2020-10-08 19:52:57 +0900

    rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,7 +69,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it 'priceの値が9,999,999より大きいと登録できない' do
-      @item.price = 10000000
+      @item.price = 10_000_000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end

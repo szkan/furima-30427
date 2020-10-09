65276b12329124812170aa9624ae742a490d0c44
szkan <71358350+szkan@users.noreply.github.com>
2020-10-09 14:38:32 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: %i[new edit]
-  before_action :set_item, only: %i[show edit update]
+  before_action :set_item, only: %i[show edit update destroy]
   before_action :move_to_index, only: :edit
 
   def index
@@ -30,6 +30,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    redirect_to root_path if @item.destroy
   end
 
   private

36a297c3e28986a5ce6191826f1a86af35ffa12d
szkan <71358350+szkan@users.noreply.github.com>
2020-10-09 19:39:12 +0900

    モデル関連作成

---
+++ app/models/buyer.rb
@@ -0,0 +1,11 @@
+class Buyer < ApplicationRecord
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
+
+  belongs_to :order
+end
